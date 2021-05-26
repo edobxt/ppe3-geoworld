790a10922e4e466db9b2630d273f1ae990f88c7a
kanjo <kanjo.school@gmail.com>
2021-05-26 17:16:34 -0400

    Simplification partie élève

--- world-intro-bt4/student.php
+++ world-intro-bt4/student.php
@@ -1,51 +1,48 @@
 <?php  
-  // Ajout du header
-  require_once 'header.php';
-  // Lien vers les méthodes
-  require_once 'inc/manager-db.php';
-
-  if (isset($_REQUEST["idUsers"]))
-  {
-    $idUsers = $_REQUEST["idUsers"];
-  }
+    // Ajout du header
+    require_once 'header.php';
+    // Lien vers les méthodes
+    require_once 'inc/manager-db.php';
 ?>
 
 <main role="main" class="flex-shrink-0">
-  <div class="container">
-  <!-- Entête -->
-    <center>
-      <h1>Partie Élève</h1>
-      <button type="button" class="btn btn-primary">See infos</button>
-    </center>
-    <br>
-    <div class="row">
-      <!-- Lien vers la création de requête -->
-      <div class="col-sm-6">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Create a query</h5>
-            <p class="card-text">Create and test SQL queries.</p>
-            <a href="make_query.php" class="btn btn-primary">Create</a>
-          </div>
+    <div class="container">
+        <center>
+            <h1 style="color:#17A589;">Welcome to the Student's area</h1>
+        </center>
+        <br>
+        <div class="row">
+            <style>
+                a {
+                    color: #000;
+                }
+                a:hover {
+                    color: #000;
+                }
+            </style>
+            <div class="col-sm-6">
+                <div class="card" style="width: 30rem; margin: 5px; height: 130px;">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="index.php">Observe the data</a></h5>
+                        <h5><small>Click to consult the geopolitical and economic data of the planet.</small></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card" style="width: 30rem; margin: 5px; height: 130px;">
+                    <div class="card-body">
+                        <h5 class="card-title">Test public queries</h5>
+                        <h5><small>Not yet implemented.</small></h5>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <!-- Lien vers la gestion des requêtes -->
-      <div class="col-sm-6">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Manage queries</h5>
-            <p class="card-text">Manage your SQL queries.</p>
-            <a href="#" class="btn btn-primary">Manage</a>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
 </main>
 
 <?php
-  // Ajout de script Javascript
-  require_once 'javascripts.php';
-  // Ajout du footer
-  require_once 'footer.php';
+    // Ajout de script Javascript
+    require_once 'javascripts.php';
+    // Ajout du footer
+    require_once 'footer.php';
 ?>
